00a5bbbc658fb223032023f845cf3558c2ec19a2
fyy <fyy@MacBook-Pro-fyy.local>
2022-07-09 15:02:34 +0300

    fix: Фикс бага с анимацией, когда резко щелкаешь по меню

--- src/components/carousel/burger.js
+++ src/components/carousel/burger.js
@@ -3,6 +3,18 @@ const menuMobile = document.querySelector('.filter-menu')
 
 export function toggelMenuMobile() {
   triggerBurger.addEventListener('click', (e) => {
+    // Фикс бага с анимацией, когда резко щелкаешь на кнопку меню
+    // (до этого она не успевала завершиться)
+    if (menuMobile.classList.contains('animation-menu')) {
+      menuMobile.querySelectorAll('li').forEach(e => {
+        e.style.transition = 'all 0.6s ease-in-out 0s'
+      })
+    } else {
+      menuMobile.querySelectorAll('li').forEach(e => {
+        e.style.transition = ''
+      })
+    }
+    
     menuMobile.classList.toggle('animation-menu')
     triggerBurger.classList.toggle('activeted-burger')
   })
